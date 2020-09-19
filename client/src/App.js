c274b3d05ad3e7747276fc3e97bdf3c0b3bcf34b
Brian Ma <jingjerma@gmail.com>
2020-09-19 15:43:20 -0700

    linked express server to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Testing frontend with Express/Node and MongoDB</h1>
-      <form method="post" action="/addname">
-        <label>Enter your name</label><br/>
-        <input type="text" name="firstName" placeholder="Enter first name..." required/>
-        <input type="text" name="lastName" placeholder="Enter last name..." required/>
-        <input type="submit" value="Add Name"/>
-      </form>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: null};
+    }
+
+    // If our Express server has been hooked up
+    componentDidMount() {
+        // res should be a JSON
+        this.callAPI()
+            .then(res => this.setState({ data: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    // Fetches our GET route from the Express server
+    callAPI = async () => {
+        // Fetch response
+        const response = await fetch('/api');
+        // Get response as JSON 
+        const body = await response.json();
+        console.log(body);
+
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body;
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <h1>{this.state.data}</h1>
+            <form method="post" action="/addname">
+                <label>Enter your name</label><br/>
+                <input type="text" name="firstName" placeholder="Enter first name..." required/>
+                <input type="text" name="lastName" placeholder="Enter last name..." required/>
+                <input type="submit" value="Add Name"/>
+            </form>
+            </div>
+        )
+    }
 }
 
 export default App;

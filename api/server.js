d4ef86ea5dd51d55808a1b7210523e8668bd4aec
Brian Ma <jingjerma@gmail.com>
2020-09-19 17:32:14 -0700

    removed dependencies

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 // Create express instance
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 8080;
 
-// Check if server is running
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+
+
 
 // Create GET route; we will fetch it from our client side app
 app.get('/api', (req, res) => {
     res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
+});
+
+
+// Check if server is running
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
